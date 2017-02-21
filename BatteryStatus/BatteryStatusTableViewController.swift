5068328bf9d77ce0c5197503f6dc03333b5ff37d
Patrick <patrick@helmsdeep.at>
2017-02-21 13:45:22 +0100

    updated documentation

--- BatteryStatus/BatteryStatusTableViewController.swift
+++ BatteryStatus/BatteryStatusTableViewController.swift
@@ -112,13 +112,13 @@ extension BatteryStatusTableViewController: CBCentralManagerDelegate {
         if let ktkOldData = serviceData[CBUUID(string:"D00D")] {
             //print("DBG: kontakt old data: \(ktkOldData.hexEncodedString())")
             
-            // parse identifier
+            // parse identifier get byte 0 - 6
             guard let identifier = String(data: ktkOldData.subdata(in: Range(0...4)), encoding: String.Encoding.ascii) else {
                 print("ERROR: Can't parse name")
                 return
             }
             
-            // parse battery level
+            // parse battery level get byte 6
             var power: UInt8 = 0
             ktkOldData.copyBytes(to: &power, from: Range(6..<7))
             
@@ -130,11 +130,11 @@ extension BatteryStatusTableViewController: CBCentralManagerDelegate {
         if let ktkNewData = serviceData[CBUUID(string:"FE6A")] {
             //print("DBG: kontakt new data: \(ktkNewData.hexEncodedString())")
             
-            // parse battery level
+            // parse battery level get byte 4
             var power: UInt8 = 0
             ktkNewData.copyBytes(to: &power, from: Range(4..<5))
             
-            // parse identifier
+            // parse identifier get byte 6 - 9
             guard let identifier = String(data: ktkNewData.subdata(in: Range(6...9)), encoding: String.Encoding.ascii) else {
                 print("ERROR: Can't parse name")
                 return
